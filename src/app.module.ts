3002c97bc9e083cd4bde0dbf5f83ee79e725946e
Carlos Eduardo Marques Freitas <carlosmfreitas05@gmail.com>
2022-10-23 22:13:22 -0300

    chore: initial structure with prisma service

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import process from 'process';
+import { join } from 'path';
+
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+import { DatabaseModule } from '@infra/db/database.module';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
