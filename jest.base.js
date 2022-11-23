58a14399300808282c385d08eac894aab333ed39
Carlos Eduardo Marques Freitas <carlosmfreitas05@gmail.com>
2022-11-23 01:22:35 -0300

    chore: add jest sonar reporter lib to sonar

--- jest.base.js
+++ jest.base.js
@@ -1,6 +1,8 @@
 module.exports = {
   collectCoverage: false,
 
+  testResultsProcessor: 'jest-sonar-reporter',
+
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
   transform: {
     '^.+\\.(t|j)sx?$': ['@swc/jest'],
