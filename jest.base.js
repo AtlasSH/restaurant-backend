315e93ca63444c594c3c610ed195b4f69a1584f5
Carlos Eduardo Marques Freitas <carlosmfreitas05@gmail.com>
2022-11-23 01:00:04 -0300

    chore: refactor sonar coverage exclusions and remove jest sonar reporter

--- jest.base.js
+++ jest.base.js
@@ -1,18 +1,6 @@
 module.exports = {
   collectCoverage: false,
 
-  reporters: [
-    'default',
-    [
-      '@casualbot/jest-sonar-reporter',
-      {
-        relativePaths: true,
-        outputName: 'sonar-report.xml',
-        outputDirectory: 'coverage',
-      },
-    ],
-  ],
-
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
   transform: {
     '^.+\\.(t|j)sx?$': ['@swc/jest'],
