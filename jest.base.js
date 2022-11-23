1a6b664d800502cbe832c0834b82505e0fbd87b8
Carlos Eduardo Marques Freitas <carlosmfreitas05@gmail.com>
2022-11-23 00:13:44 -0300

    chore: collect sonar coverage from jest sonar reporter lib

--- jest.base.js
+++ jest.base.js
@@ -1,6 +1,18 @@
 module.exports = {
   collectCoverage: false,
 
+  reporters: [
+    'default',
+    [
+      '@casualbot/jest-sonar-reporter',
+      {
+        relativePaths: true,
+        outputName: 'sonar-report.xml',
+        outputDirectory: 'coverage',
+      },
+    ],
+  ],
+
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
   transform: {
     '^.+\\.(t|j)sx?$': ['@swc/jest'],
